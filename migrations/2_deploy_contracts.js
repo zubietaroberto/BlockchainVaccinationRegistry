66f655df6edb9962990720a7cd62f57f0205adc9
Roberto E. Zubieta <zubieta.roberto.e@gmail.com>
2019-12-11 08:56:15 -0500

    Removed unused Contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
-var IdentityToken = artifacts.require("./ERC1155.sol");
 var IdentityTokenMintable = artifacts.require("./ERC1155Mintable.sol");
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(IdentityToken);
   deployer.deploy(IdentityTokenMintable);
 };
